9affc4220112f177a2a0506dbcbee50727d7e00c
Abubeker Taha <abubekertaha3@gmail.com>
2025-06-20 10:42:10 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl text-blue-500'>Hello Coders</h1>
+    </>
+  )
+}
+
+export default App
